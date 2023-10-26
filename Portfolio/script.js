2e2c0ec2cfca6048f2addaf7294761826699ad76
Tejaswi-bejawada <148537717+Tejaswi-bejawada@users.noreply.github.com>
2023-10-26 18:43:34 +0530

    portfolio first commit

---
+++ Portfolio/script.js
@@ -0,0 +1,19 @@
+// JavaScript code for smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust for the navigation bar height
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+       
